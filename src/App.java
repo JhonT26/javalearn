74af05b2d16bcff79aca1ec31443eb7f27e5cb3b
JhonT26 <jhont26@gmail.com>
2021-06-29 21:09:39 -0500

    Second Class Java Basic

--- src/App.java
+++ src/App.java
@@ -1,5 +1,55 @@
 public class App {
+
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //Operadores Aritmeticos
+        String resultado = "Hola";
+        int resultado1 = 15 + 32;
+        int resultado2 = 125 - 32;
+        double resultado3 = 25 / 3;
+        float resultado4 = 58 * 21;
+        int resultado5 = 21 % 9;
+
+        //Operadores Unarios
+        resultado1++;
+        resultado2--;
+        resultado = -resultado;
+        resultado = +resultado;
+        
+        //Operadores Lógicos
+
+        boolean = respuesta = true;
+        respuesta = 5 > 8;
+        respuesta = 5 < 8;
+        respuesta = 5 <= 8;
+        respuesta = 5 >= 8;
+        respuesta = 5 != 8;
+        respuesta = 5 == 8;
+        respuesta = 12 < 5 && 4 > 1;
+        respuesta = 14 < 25 || 1 > 5;
+
+        //Operadores bit a bit
+        //&
+        //^
+        //|
+        //Estructuras de Control
+
+        if (condition) {
+            // Bloque de código
+        }
+        if (condition) {
+           // Bloque de código si se cumple 
+        } else {
+           // Bloque de código si no se cumple  
+        }
+
+
     }
 }
